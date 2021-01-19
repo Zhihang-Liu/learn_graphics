942f43bd43dc1a81dfb188ee7c7673e845b20fa2
Lyzh <enterindex@gmail.com>
2021-01-19 16:34:14 +0800

    add sampling pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use glam::*;
-use bmp::{Image, consts::{BLACK, WHITE}};
+use bmp::{Image, Pixel, consts::{WHITE}};
 use ordered_float::OrderedFloat;
+use lazy_static::lazy_static;
 
 type Triangle = Mat3;
 
@@ -37,23 +38,64 @@ fn bounding_box(tri: Triangle) -> Mat2 {
     const_mat2!([x_min, y_min], [x_max, y_max])
 }
 
-fn rasterization(tri: Triangle, screen: &mut Image) {
+
+fn rasterization(tri: Triangle, buffer: &mut Vec<Vec<bool>>) {
     let size = bounding_box(tri);
-    for y in size.x_axis.y as u32..size.y_axis.y as u32+1 {
-        for x in size.x_axis.x as u32..size.y_axis.x as u32+1 {
-            if screen.get_pixel(x, y) == BLACK {
-                let r = inside(tri, x as f32+0.5, y as f32+0.5);
-                let r = if r { WHITE } else { BLACK };
-                screen.set_pixel(x, y, r);
+    for y in size.x_axis.y as usize..size.y_axis.y as usize+1 {
+        for x in size.x_axis.x as usize..size.y_axis.x as usize+1 {
+            let p = buffer.get_mut(y).unwrap().get_mut(x).unwrap();
+            if *p == false {
+                *p = inside(tri, x as f32+0.5, y as f32+0.5);
             };
         }
     }
 }
 
-fn main() {
-    // input
-    let hyper: f32 = 100.0;
-    let size = uvec2(32*hyper as u32, 32*hyper as u32);
+/*
+#[inline]
+fn box_filter(i: Mat3) -> f32 {
+    let rv: Vec3 = i.x_axis + i.y_axis + i.z_axis;
+    let rv = rv.x + rv.y + rv.z;
+    rv / 9.0
+} */
+
+
+
+lazy_static! {
+    static ref WHITE_VEC: Vec3 = vec3(WHITE.r as f32, WHITE.g as f32, WHITE.b as f32);
+}
+
+fn sampling_unit(i: &Vec<bool>) -> Vec3 {
+    let r: f32 = i.iter().map(|x|*x as u8 as f32).sum();
+    *WHITE_VEC * r / i.len() as f32
+}
+
+fn sampling(i: Vec<Vec<bool>>, sampling_pipe: usize, h: usize, w: usize) -> Vec<Vec<Vec3>> {
+    let mut buffer = new_2d_array(Vec3::zero(), w, h);
+    for y in 0..h {
+        let hy = y*sampling_pipe;
+        for x in 0..w {
+            let hx = x*sampling_pipe;
+            let slice_i: Vec<bool> = Vec::from(&i[hy..hy+sampling_pipe])
+                .iter()
+                .flat_map(|x| Vec::from(&x[hx..hx+sampling_pipe]))
+                .collect();
+            let r = sampling_unit(&slice_i);
+            let p = unsafe {
+                buffer.get_unchecked_mut(y).get_unchecked_mut(x)
+            };
+            *p = r;
+        }
+    }
+    buffer
+}
+
+#[inline]
+fn vec3_to_pixel(i: Vec3) -> Pixel {
+    Pixel::new(i.x as u8, i.y as u8, i.z as u8)
+}
+
+fn init() -> Vec<Mat3> {
     let lst: [[f32; 3]; 3] = [
         [2.2, 1.3, 1.0],
         [4.4, 11.0, 1.0],
@@ -64,10 +106,39 @@ fn main() {
         [15.4, 20.0, 1.0],
         [25.3, 24.6, 1.0]];
     let tri1 = Mat3::from_cols_array_2d(&lst);
-    let mut screen = Image::new(size.x, size.y);
-    [tri, tri1]
+    vec![tri, tri1]
+}
+
+fn new_2d_array<T: Clone>(default: T, x: usize, y: usize) -> Vec<Vec<T>> {
+    let line = vec![default; x];
+    vec![line; y]
+}
+
+fn output(path: &str, buffer: &Vec<Vec<Vec3>>, x: u32, y: u32) {
+    let mut screen = Image::new(x, y);
+    for (y, l) in buffer.iter().enumerate() {
+        for (x, p) in l.iter().enumerate() {
+            screen.set_pixel(x as u32, y as u32, vec3_to_pixel(*p));
+        }
+    }
+    screen.save(path).expect("save error");
+}
+
+fn main() {
+    // input
+    const HYPER: usize = 10;
+    let sampling_pipe = 4;
+    let size = uvec2(32, 32);
+
+    let mut buffer = new_2d_array(
+        false, size.x as usize*sampling_pipe*HYPER, size.y as usize*sampling_pipe*HYPER);
+
+    init()
         .iter()
-        .map(|x| x.mul_scalar(hyper))
-        .for_each(|tri| rasterization(tri, &mut screen));
-    screen.save("out.bmp").expect("save error");
+        .map(|x| x.mul_scalar(HYPER as f32 * sampling_pipe as f32))
+        .for_each(|tri| rasterization(tri, &mut buffer));
+    
+    let buffer = sampling(buffer, sampling_pipe, size.y as usize*HYPER, size.x as usize*HYPER);
+
+    output("out.bmp", &buffer, HYPER as u32*size.x, HYPER as u32*size.y);
 }
